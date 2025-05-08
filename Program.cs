d1549210c7da248bf008bb0e13ed5b54ae6cb57e
Taylo <Taylo@MARIEL>
2025-05-08 14:05:00 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Display individual digits using recursion
+
+using System;
+
+class Program
+{
+
+    static void Digits(int n)
+    {
+            if (n < 10)
+            {
+                Console.WriteLine(n);
+                return;
+            }
+             Digits(n / 10);
+        Console.WriteLine(n % 10);
+
+        }
+        static void Main(string[] args)
+        {
+        int number = 1234;
+            Console.WriteLine("The digits in the {number} are: ");
+            Digits(number);
+
+        }
+    }
+
